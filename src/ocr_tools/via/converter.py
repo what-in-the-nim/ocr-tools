9060052a59c4812c11491a5748e666aac78f7d06
Win <chatcharinsang@gmail.com>
2024-06-09 17:22:39 +0700

    Add converter

---
+++ src/ocr_tools/via/converter.py
@@ -0,0 +1,12 @@
+from pdf2image import convert_from_bytes, convert_from_path
+from PIL import Image
+
+
+def pdf_to_images(pdf_path: str) -> list[Image.Image]:
+    images = convert_from_path(pdf_path)
+    return images
+
+
+def bytes_to_image(pdf_bytes: bytes, dpi: int = 400, num_thread: int = 1) -> list[Image.Image]:
+    images = convert_from_bytes(pdf_bytes, dpi=dpi, num_thread=num_thread)
+    return images
