776b0ec3a17dfd7ac316b176de5a1315a0b6ea32
Win <chatcharinsang@gmail.com>
2024-06-09 17:27:37 +0700

    Update converter

--- src/ocr_tools/via/converter.py
+++ src/ocr_tools/via/converter.py
@@ -7,6 +7,8 @@ def pdf_to_images(pdf_path: str) -> list[Image.Image]:
     return images
 
 
-def bytes_to_image(pdf_bytes: bytes, dpi: int = 400, num_thread: int = 1) -> list[Image.Image]:
-    images = convert_from_bytes(pdf_bytes, dpi=dpi, num_thread=num_thread)
+def bytes_to_image(
+    pdf_bytes: bytes, dpi: int = 400, num_thread: int = 1
+) -> list[Image.Image]:
+    images = convert_from_bytes(pdf_bytes, dpi=dpi, thread_count=num_thread)
     return images
